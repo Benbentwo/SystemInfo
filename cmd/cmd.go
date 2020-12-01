983cb010c5f7304bbb2c17091f25a426b04e68c1
Benbentwo <Benbentwo@users.noreply.github.com>
2020-12-01 22:33:31 +0000

    Make this mine

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,8 +1,8 @@
 package cmd
 
 import (
-	"github.com/Benbentwo/go-bin-generic/pkg/common"
-	"github.com/Benbentwo/go-bin-generic/pkg/version"
+	"github.com/Benbentwo/Windows10BootStrapper/pkg/common"
+	"github.com/Benbentwo/Windows10BootStrapper/pkg/version"
 	"github.com/spf13/viper"
 	"io"
 	"strings"
